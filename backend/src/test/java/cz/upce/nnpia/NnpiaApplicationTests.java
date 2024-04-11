8adc12cc06c57fbb29103cf1cbe690528631d1a9
Pokorný <st58594@student.upce.cz>
2024-04-11 16:04:30 +0200

    Initial commit

---
+++ backend/src/test/java/cz/upce/nnpia/NnpiaApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.upce.nnpia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NnpiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
